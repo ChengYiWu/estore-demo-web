4ebd7582c240f786ca0de0972d1a54e1281fbcc6
chris_cy_wu <a092816190823@gmail.com>
2023-11-16 22:31:15 +0800

    add vite copy file process

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, normalizePath } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: normalizePath(path.resolve(__dirname, './staticwebapp.config.js')),
+          dest: "./",
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
