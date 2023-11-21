3e08844545f595e2d1c5167e35273346e3214e51
chris_cy_wu <a092816190823@gmail.com>
2023-11-21 22:55:55 +0800

    fix loading mask

--- src/layouts/AdminLayout.content.tsx
+++ src/layouts/AdminLayout.content.tsx
@@ -1,34 +1,18 @@
 import { Spin, Layout } from "antd";
-import { createStyles } from "antd-style";
 import { Outlet } from "react-router-dom";
 import useStore from "@/store/store";
 
 const { Content } = Layout;
 
-const useStyle = createStyles(({ token }) => ({
-  contentLoading: {
-    "&.ant-spin-spinning": {
-      position: "absolute",
-      inset: 0,
-      zIndex: token.zIndexPopupBase,
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      background: "rgba(255, 255, 255, .6)",
-    },
-  },
-}));
-
 const AdminLayoutContent = () => {
-  const { styles } = useStyle();
-
   // 檢查 store 是否有任何要 loading 的狀態
   const isContentLoading = useStore((state) => state.contentLoading);
 
   return (
     <Content>
-      <Spin spinning={isContentLoading} className={styles.contentLoading} />
-      <Outlet />
+      <Spin spinning={isContentLoading}>
+        <Outlet />
+      </Spin>
     </Content>
   );
 };
