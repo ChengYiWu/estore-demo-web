7666d41ed19e1850ac650b0b029fe0289f0db68a
chris_cy_wu <a092816190823@gmail.com>
2023-11-21 23:09:16 +0800

    fix layout content height

--- src/layouts/AdminLayout.content.tsx
+++ src/layouts/AdminLayout.content.tsx
@@ -1,16 +1,27 @@
 import { Spin, Layout } from "antd";
+import { createStyles } from "antd-style";
 import { Outlet } from "react-router-dom";
 import useStore from "@/store/store";
 
 const { Content } = Layout;
 
+const useStyle = createStyles(() => ({
+  contentLoading: {
+    height: "100%",
+    "& .ant-spin-container": {
+      height: "100%",
+    },
+  },
+}));
+
 const AdminLayoutContent = () => {
+  const { styles } = useStyle();
   // 檢查 store 是否有任何要 loading 的狀態
   const isContentLoading = useStore((state) => state.contentLoading);
 
   return (
     <Content>
-      <Spin spinning={isContentLoading}>
+      <Spin spinning={isContentLoading} wrapperClassName={styles.contentLoading}>
         <Outlet />
       </Spin>
     </Content>
